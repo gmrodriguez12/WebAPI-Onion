d86e26d3abfd1d3a01dd5c6bf414456b4cca3377
DESKTOP-CMM6F5S\Gonzalo-Rodriguez <gmrodriguez12@gmail.com>
2022-05-10 14:38:06 -0300

    Product Entity based on Production.Product AdventureWorks DB

---
+++ Domain/Common/AuditoryEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public abstract class AuditoryEntity
+    {
+        public virtual int Id { get; set; }        
+        public DateTime ModifiedDate { get; set; } 
+    }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+        public bool? MakeFlag { get; set; }
+        public bool? FinishGoodFlag { get; set; }
+        public string Color { get; set; }
+        public short SafetyStockLevel { get; set; }
+        public short ReorderPoint { get; set; }
+        public decimal StandardCost { get; set; }
+        public decimal ListPrice { get; set; }
+        public int DaysToManufacture { get; set; }
+        public int IdVendor { get; set; }
+        public string VendorName { get; set; }
+        public int IdSubCategory { get; set; }
+        public string SubCategoryName { get; set; }
+        public int IdCategory { get; set; }
+        public string CategoryName { get; set; }
+    }
+}

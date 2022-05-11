5e2794ae4e10068addff09197b535e8799a3fdb7
DESKTOP-CMM6F5S\Gonzalo-Rodriguez <gmrodriguez12@gmail.com>
2022-05-11 10:11:23 -0300

    API Endpoint

---
+++ WebAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class BaseApiController : Controller
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>(); 
+    }
+}
---
+++ WebAPI/Controllers/v1/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Features.Products.Commands.CreateProductCommand;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.v1
+{
+
+    [ApiVersion("1.0")]
+    public class ProductController : BaseApiController
+    {
+        //POST api/<controller>
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductCommand product)
+        {
+            return Ok(await Mediator.Send(product));
+        }
+    }
+}

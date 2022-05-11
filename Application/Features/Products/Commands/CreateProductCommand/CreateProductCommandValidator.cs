bd0025f9141c5e1fcac2272fe96c5e6add87cf4a
DESKTOP-CMM6F5S\Gonzalo-Rodriguez <gmrodriguez12@gmail.com>
2022-05-11 09:20:04 -0300

    Fluent validation

---
+++ Application/Features/Products/Commands/CreateProductCommand/CreateProductCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Products.Commands.CreateProductCommand
+{
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().WithMessage("{PropertyName} is required")
+                                .MaximumLength(50).WithMessage("{PropertyName} maximum length is {MaxLength}");
+
+            RuleFor(p => p.ProductNumber).NotEmpty().WithMessage("{PropertyName} is required")
+                               .MaximumLength(25).WithMessage("{PropertyName} maximum length is {MaxLength}");
+
+            RuleFor(p => p.SafetyStockLevel).NotEmpty().WithMessage("{PropertyName} is required");
+
+            RuleFor(p => p.ReorderPoint).NotEmpty().WithMessage("{PropertyName} is required");
+
+            RuleFor(p => p.ListPrice).NotEmpty().WithMessage("{PropertyName} is required");
+
+            RuleFor(p => p.IdVendor).NotEmpty().WithMessage("{PropertyName} is required");
+
+            RuleFor(p => p.IdSubCategory).NotEmpty().WithMessage("{PropertyName} is required");
+
+            RuleFor(p => p.IdCategory).NotEmpty().WithMessage("{PropertyName} is required");
+        }
+    }
+}

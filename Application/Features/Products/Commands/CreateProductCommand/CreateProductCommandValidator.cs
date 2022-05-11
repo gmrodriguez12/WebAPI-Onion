b664232ff56eaa9398b985c4f4de8de519a7851e
DESKTOP-CMM6F5S\Gonzalo-Rodriguez <gmrodriguez12@gmail.com>
2022-05-11 12:32:16 -0300

    update validations

--- Application/Features/Products/Commands/CreateProductCommand/CreateProductCommandValidator.cs
+++ Application/Features/Products/Commands/CreateProductCommand/CreateProductCommandValidator.cs
@@ -6,23 +6,24 @@ namespace Application.Features.Products.Commands.CreateProductCommand
     {
         public CreateProductCommandValidator()
         {
-            //RuleFor(p => p.Name).NotEmpty().WithMessage("{PropertyName} is required")
-            //                    .MaximumLength(50).WithMessage("{PropertyName} maximum length is {MaxLength}");
+            RuleFor(p => p.Name).NotEmpty().WithMessage("{PropertyName} is required")
+                                .MaximumLength(50).WithMessage("{PropertyName} maximum length is {MaxLength}");
 
-            //RuleFor(p => p.ProductNumber).NotEmpty().WithMessage("{PropertyName} is required")
-            //                   .MaximumLength(25).WithMessage("{PropertyName} maximum length is {MaxLength}");
+            RuleFor(p => p.ProductNumber).NotEmpty().WithMessage("{PropertyName} is required")
+                               .MaximumLength(25).WithMessage("{PropertyName} maximum length is {MaxLength}");
 
-            //RuleFor(p => p.SafetyStockLevel).NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(p => p.ProductNumber)
+             .NotEmpty().WithMessage("{PropertyName} is required")
+             .Matches(@"^\s{2}-\d{4}$").WithMessage("{PropertyName} expected format SS-0000")
+             .MaximumLength(9).WithMessage("{PropertyName} maximun lenght is {MaxLength}");
 
-            //RuleFor(p => p.ReorderPoint).NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(p => p.SafetyStockLevel).NotEmpty().WithMessage("{PropertyName} is required");
 
-            //RuleFor(p => p.ListPrice).NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(p => p.ReorderPoint).NotEmpty().WithMessage("{PropertyName} is required");
 
-            //RuleFor(p => p.IdVendor).NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(p => p.ListPrice).NotEmpty().WithMessage("{PropertyName} is required");
 
-            //RuleFor(p => p.IdSubCategory).NotEmpty().WithMessage("{PropertyName} is required");
-
-            //RuleFor(p => p.IdCategory).NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(p => p.DaysToManufacture).NotEmpty().WithMessage("{PropertyName} is required");
         }
     }
 }
